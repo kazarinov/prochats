2f60fb43c11a74c5ae2ff09aae2c0b16942cb7b2
govsyannikov <ovsyanng@gmail.com>
2015-05-09 21:13:32 +0300

    getting vk messages

--- prochats/views/client.py
+++ prochats/views/client.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
-import datetime
+import hashlib
+import random
+import sys
 
+import vk
 from .. import db, app
 from ..models.users import User
 from .rendering import to_json, get_renderer
 from .validators import (
     accept,
     param_string,
-    param_float,
     param_int,
     param_sdk_token,
 )
-import hashlib, random, sys, datetime
 
 
 renderer = get_renderer()
@@ -24,21 +25,52 @@ renderer = get_renderer()
 def register(vk_id):
     # Генерируем токен уникального приложения
     sdk_token = hashlib.md5(str(random.randint(0, sys.maxint)))
-    while (User.query.filter_by(sdk_token=sdk_token).first()):
+    while User.query.filter_by(sdk_token=sdk_token).first():
         sdk_token = hashlib.md5(str(random.randint(0, sys.maxint)))
     new_user = User(vk_token=vk_id, sdk_token=sdk_token)
     db.session.add(new_user)
     db.session.commit()
 
+
+def get_vk_messages(vk_token, chat_id, timestamp=None):
+    vk_api = vk.API(access_token=vk_token, timeout=5)
+
+    chunk = 200
+    result_messages = []
+
+    while True:
+        history = vk_api.messages.getHistory(chat_id=chat_id, count=chunk)
+
+        for item in history.get('items'):
+            if (timestamp is not None and item.get('date') < timestamp) \
+                    or item.get('read_state') == 1 and timestamp is None:
+                break
+
+            result_messages.append({
+                'body': item.get('body'),
+                'message_id': item.get('id'),
+                'date': item.get('date'),
+                'read_state': item.get('read_state'),
+            })
+
+        if history.get('count') < chunk:
+            break
+
+    return result_messages
+
+
 @app.route("/tags", methods=["GET"])
 @to_json
 @accept(
     param_sdk_token(),
-    param_int('chat_id', forward='chat_id'),
-    param_int('message_id', required=None, forward='last_message_id')
+    param_int('chat_id'),
+    param_int('timestamp', required=None)
 )
-def get_tags(application, chat_id, last_message_id):
-    # Вернуть теги
+def get_tags(user, chat_id, timestamp):
+    # получить пачку непрочитанных сообщений
+    messages = get_vk_messages(user.vk_token, chat_id)
+
+    # TODO: aggregate tags
     pass
 
 @app.route("/messages", methods=["GET"])
